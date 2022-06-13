0ba487fa16ff7f17cdb14ba9b9d2727de2188419
Waqas Pathan <waqaspathan00@gmail.com>
2022-06-13 00:31:16 -0400

    created testable api route

--- pages/api/date.py
+++ pages/api/date.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
-class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+class handler(BaseHTTPRequestHandler):
+    """ this route does NOT work, but it should trigger at localhost:3000/api/date"""
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        return
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,25 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    async function makeRequest() {
+    async function makeRequestApi() {
         const response = await fetch("api/")
         const data = await response.json()
         // console.log(data[0].food)
         console.log(data)
     }
 
+    async function makeRequestDate() {
+        const response = await fetch("api/date")
+        const data = await response.text()
+        console.log(data)
+    }
+
+
+
     return (
         <div>
-            <button onClick={makeRequest}>Make request to api/index</button>
-            <button onClick={makeRequest}>Make request to api/date</button>
+            <button onClick={makeRequestApi}>Make request to api/</button>
+            <button onClick={makeRequestDate}>Make request to api/date</button>
         </div>
     )
 }
