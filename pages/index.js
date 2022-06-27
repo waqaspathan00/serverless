4b17f77e7e8185f81c7f608fed7ef138ed17fa98
Waqas Pathan <waqaspathan00@gmail.com>
2022-06-27 00:51:26 -0400

    code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 // submit a food and you should see "successfully submitted x"
 
 export default function Home() {
+    const [food, setFood] = useState();
+
     async function makeRequestApi() {
 
         const response = await fetch('https://us-central1-learnfirebase-544b9.cloudfunctions.net/get_food?food=pizza')
-        console.log(response)
         const data = await response.json()
-        console.log(data.food)
+        setFood(data.food)
     }
 
     return (
         <div>
             <button onClick={makeRequestApi}>Make request to api/</button>
+            {food && <div>Successfully submitted for {food}</div>}
         </div>
     )
 }
